96f3a033bc9a6a21bae4d77c7bd4bfaa9f9d0c72
Spencer Millett <millett.spencer@gmail.com>
2019-11-14 14:27:51 -0700

    Fixed stuff

--- BlowOutProject/Controllers/RentalsController.cs
+++ BlowOutProject/Controllers/RentalsController.cs
@@ -12,7 +12,12 @@ namespace BlowOutProject.Controllers
 
         public static List<Instrument> lstInstruments = new List<Instrument>()
         {
-            new Instrument { Name = "Trumpet", PriceUsed = "$55 a month", PriceNew = "$25 a month" }
+            new Instrument { Name = "Trumpet", PriceUsed = "$25 a month", PriceNew = "$55 a month" },
+            new Instrument { Name = "Trombone", PriceUsed = "$35 a month", PriceNew = "$60 a month" },
+            new Instrument { Name = "Tuba", PriceUsed = "$50 a month", PriceNew = "$70 a month" },
+            new Instrument { Name = "Flute", PriceUsed = "$25 a month", PriceNew = "$40 a month" },
+            new Instrument { Name = "Clarinet", PriceUsed = "$27 a month", PriceNew = "$35 a month" },
+            new Instrument { Name = "Saxophone", PriceUsed = "$30 a month", PriceNew = "$42 a month" }
         };
         
         // GET: Rentals
@@ -21,9 +26,10 @@ namespace BlowOutProject.Controllers
             return View();
         }
 
-        public ActionResult Details()
+        public ActionResult Details(string name)
         {
-            return View();
+            Instrument theInst = lstInstruments.Find(x => x.Name == name);
+            return View(theInst);
         }
     }
 
