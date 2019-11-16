4b0eb03850323eab53784dcd0b455cbec9819dd4
Spencer Millett <millett.spencer@gmail.com>
2019-11-15 17:57:34 -0700

    Stuff

--- BlowOutProject/Models/Instrument.cs
+++ BlowOutProject/Models/Instrument.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace BlowOutProject.Models
     public class Instrument
     {
         public string Name { get; set; }
+
+        [Display(Name = "Used Price")]
         public string PriceUsed { get; set; }
+        [Display(Name = "New Price")]
         public string PriceNew { get; set; }
 
         public string CalcPriceUsed()
